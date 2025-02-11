3c6ad5cd233871ce40682b34c054419588750471
BastelBaus <44684710+BastelBaus@users.noreply.github.com>
2025-02-12 00:44:02 +0100

    further merges

--- app/server/network.py
+++ app/server/network.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 
 def get_route_to(addr:str) -> str:
-    return __call_command("ip --json route get {addr}")
+    return __call_command(f"ip --json route get {addr}")
 
 def get_interfaces() -> str:
     return __call_command("ip  -j -br address")
@@ -20,11 +20,6 @@ def __call_command(cmd:str) -> dict:
     stream = os.popen(cmd)
     output = stream.read()
     logger.info(f"returned:\n{output}")
-<<<<<<< HEAD
-    return json.loads(output)
-=======
+
     if len(output)>0: return json.loads(output)
     else:             return {}
->>>>>>> 40dc7edeb975a4c9539ba3b8488e1a7e68d5396f
-    
-
