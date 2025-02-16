67c52f77d8bbb60999df25f62a5c1aaf4322ca4d
BastelBaus <44684710+BastelBaus@users.noreply.github.com>
2025-02-16 17:02:07 +0100

    cleanup

--- app/frontend/config.py
+++ app/frontend/config.py
@@ -27,9 +27,11 @@ class FrontendConfig(Config):
         ''' check that the data is correct '''
         if not hasattr(self,"data"): self.data = {}
         if not isinstance(self.data,dict): self.data = {}
-        if "version" not in self.data.keys(): self.data["version"] = defaults.VERSION
-        if "sites"   not in self.data.keys(): self.data["sites"] = [ {'address':f'http://localhost:{self._API_PORT}'}]
+        if "version" not in self.data.keys(): # pylint: disable=consider-iterating-dictionary
+             self.data["version"] = defaults.VERSION
+        if "sites"   not in self.data.keys(): # pylint: disable=consider-iterating-dictionary
+             self.data["sites"] = [ {'address':f'http://localhost:{self._API_PORT}'}]
         for i in range(len(self.data["sites"])):
-            if "token"   not in self.data["sites"][i].keys(): self.data["sites"][i]["token"]   = ""
-            if "name"    not in self.data["sites"][i].keys(): self.data["sites"][i]["name"]    = ""
-            if "address" not in self.data["sites"][i].keys(): self.data["sites"][i]["address"] = ""
+            if "token"   not in self.data["sites"][i].keys(): self.data["sites"][i]["token"]   = "" # pylint: disable=multiple-statements
+            if "name"    not in self.data["sites"][i].keys(): self.data["sites"][i]["name"]    = "" # pylint: disable=multiple-statements
+            if "address" not in self.data["sites"][i].keys(): self.data["sites"][i]["address"] = "" # pylint: disable=multiple-statements
